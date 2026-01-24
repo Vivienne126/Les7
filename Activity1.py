1e4ec0f85c5fcd20a5e62fa7f6deaab1fe25531b
Vivienne126 <vivienne.singh@cns.ac.in>
2026-01-24 12:09:04 +0530

    New

---
+++ Activity1.py
@@ -0,0 +1,23 @@
+list1=["apple" , "guava" , "mango" , "kiwi" , "banana"]
+print(len(list1))
+
+#1st element
+print(list1[0])
+#Last element
+print(list1[-1])
+
+#Append
+list1.append("strawberry")
+print(list1)
+
+#Remove
+list1.remove("banana")
+print(list1)
+
+#Sort
+list1.sort()
+print(list1)
+
+#pop element
+list1.pop(2)
+print(list1)
